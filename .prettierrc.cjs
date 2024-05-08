658efd3ba7608c1fb037837391a8803352fb707e
kevin-sg <kevin.boster99@gmail.com>
2024-05-08 18:03:53 -0300

    feat: Initial Commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+	tabWidth: 2,
+	printWidth: 120,
+	singleQuote: true,
+	semi: true,
+	useTabs: true,
+	trailingComma: 'all',
+	jsxSingleQuote: true,
+	plugins: ['prettier-plugin-tailwindcss'],
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [{ title: 'Started project' }, { name: 'description', content: 'Initial project' }];
 };
 
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+export default function HomePage() {
+	return (
+		<div className='container mx-auto'>
+			<h1 className='text-center text-3xl font-bold'>Basic Interface Project</h1>
+		</div>
+	);
 }
